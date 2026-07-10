7fe815fb1d36d3d79c3b5475aa1d625f4cfb71ba
Gaurav Thakur <137675373+gauravthakur05@users.noreply.github.com>
2026-07-10 12:02:34 +0530

    Time: 7 ms (23.1%), Space: 12 MB (29.61%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char,int>m;
+
+        for(char c:magazine){
+            m[c]++;
+        }
+        for(char c:ransomNote){
+            m[c]--;
+            if(m[c]<0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
