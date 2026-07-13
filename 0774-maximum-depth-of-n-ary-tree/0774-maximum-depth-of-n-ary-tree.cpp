939e1ed09bbee1d90cadbea5c4cbe2a27703a341
Gaurav Thakur <137675373+gauravthakur05@users.noreply.github.com>
2026-07-13 12:48:52 +0530

    Time: 11 ms (67.29%), Space: 14.5 MB (74.89%) - LeetHub

---
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root==NULL)return 0;
+
+        int mx=0;
+
+        for(auto c:root->children){
+            mx=max(mx,maxDepth(c));
+        }
+        return mx+1;
+    }
+};
